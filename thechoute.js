df356156d54be975b1c6bbc8b762bc1cee8ddadd
Vi11egas <98242149+Vi11egas@users.noreply.github.com>
2022-01-26 18:51:40 -0600

    Update thechoute.js

--- thechoute.js
+++ thechoute.js
@@ -1469,7 +1469,7 @@ case 'menu':
                 if (isBan) return reply (baby.only.benned)	
                 reply('Cargando Menu, Porfavor no hacer spam de mensaje')
                txtt =`${cmenu}`              
-                sendButLocation(from, `${txtt}`, `Creador @${Oowner.split("@")[0]}`, fs.readFileSync("./media/imagen/absu.jpg"),
+                sendButLocation(from, `${txtt}`, `Creador @${Oowner.split("@")[0]}`, fs.readFileSync("./media/imagen/absu2.jpg"),
                 buttons =[ {buttonId:`ml`,buttonText:{displayText: 'Lista de Menus'},type:1},
                 {buttonId:`sex`,buttonText:{displayText:'Como Tener Este Bot'},type:1},
                 {buttonId:`grupo`,buttonText:{displayText:'Grupos OFC'},type:1}],
